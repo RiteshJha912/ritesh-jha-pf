50af33784f988307556600f54119b00835d8907d
RiteshJha912 <141942049+RiteshJha912@users.noreply.github.com>
2024-12-05 00:15:27 +0530

    Set default theme to dark !

--- src/common/themeContext.jsx
+++ src/common/themeContext.jsx
@@ -5,14 +5,26 @@ const themeContext = createContext()
 export const useTheme = () => useContext(themeContext)
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(() => {
-    const savedTheme = localStorage.getItem('theme')
-    return savedTheme ? savedTheme : 'dark' 
-  })
+  // Initialize theme state with dark mode by default or from localStorage
+  const [theme, setTheme] = useState('dark') // Default theme is dark
 
   useEffect(() => {
+    // Check for stored theme in localStorage when component mounts
+    const savedTheme = localStorage.getItem('theme')
+
+    if (savedTheme) {
+      setTheme(savedTheme)
+    } else {
+      // If no saved theme, set to 'dark' initially
+      localStorage.setItem('theme', 'dark')
+      setTheme('dark')
+    }
+  }, [])
+
+  useEffect(() => {
+    // Apply the theme to the document body
     document.body.setAttribute('data-theme', theme)
-    localStorage.setItem('theme', theme) 
+    localStorage.setItem('theme', theme) // Store the preference in localStorage
   }, [theme])
 
   const toggleTheme = () => {
