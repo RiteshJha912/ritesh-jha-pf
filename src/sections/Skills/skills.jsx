e3c853ad6d981ec076800ed5d15677232e347f7c
RiteshJha912 <141942049+RiteshJha912@users.noreply.github.com>
2024-12-15 16:46:16 +0530

    small changes

--- src/sections/Skills/skills.jsx
+++ src/sections/Skills/skills.jsx
@@ -13,20 +13,20 @@ function skills() {
     <section id='skills' className={styles.container}>
       <h1 className='sectionTitle'>Skills 🧠</h1>
 
-      <div className={styles.skillList} data-aos='zoom-in'>
+      <div className={styles.skillList} data-aos='zoom-out'>
         <SkillList skill='OSINT' />
         <SkillList skill='Network Security' />
       </div>
       <hr data-aos='zoom-in' />
 
-      <div className={styles.skillList} data-aos='zoom-in'>
+      <div className={styles.skillList} data-aos='zoom-out'>
         <SkillList skill='MongoDB' />
         <SkillList skill='MySQL' />
         <SkillList skill='PostgreSQL' />
       </div>
       <hr data-aos='zoom-in' />
 
-      <div className={styles.skillList} data-aos='zoom-in'>
+      <div className={styles.skillList} data-aos='zoom-out'>
         <SkillList skill='C++' />
         <SkillList skill='Java' />
         <SkillList skill='Python' />
@@ -35,7 +35,7 @@ function skills() {
       </div>
       <hr data-aos='zoom-in' />
 
-      <div className={styles.skillList} data-aos='zoom-in'>
+      <div className={styles.skillList} data-aos='zoom-out'>
         <SkillList skill='Git' />
         <SkillList skill='Postman' />
         <SkillList skill='Firebase' />
@@ -44,13 +44,17 @@ function skills() {
       </div>
       <hr data-aos='zoom-in' />
 
-      <div className={styles.skillList} data-aos='zoom-in'>
+      <div className={styles.skillList} data-aos='zoom-out'>
         <SkillList skill='HTML' />
         <SkillList skill='CSS' />
         <SkillList skill='React.js' />
         <SkillList skill='Three.js' />
         <SkillList skill='Node.js' />
         <SkillList skill='Express.js' />
+ 
+      </div>
+      <div className={styles.skillList} data-aos='zoom-out '>
+
         <SkillList skill='Pug.js' />
         <SkillList skill='Flask' />
         <SkillList skill='REST API' />
