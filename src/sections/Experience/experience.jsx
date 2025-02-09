74bc9787055ca61867eb10659a571c8ffe67f03f
RiteshJha912 <141942049+RiteshJha912@users.noreply.github.com>
2025-02-09 21:43:25 +0530

    updated work

--- src/sections/Experience/experience.jsx
+++ src/sections/Experience/experience.jsx
@@ -1,32 +1,39 @@
 import React, { useEffect } from 'react'
 import AOS from 'aos'
-import 'aos/dist/aos.css' 
+import 'aos/dist/aos.css'
 import styles from './experienceStyles.module.css'
 import InternshipCard from '../../common/internshipCard'
 import deepCytesLogo from '../../assets/deepcytes.jpg'
 import stallionLogo from '../../assets/TSP.jpg'
+import noxAlgologo from '../../assets/noxalgo.jpg'
 
 function Internships() {
   useEffect(() => {
     AOS.init({
-      duration: 900, 
+      duration: 900,
       easing: 'ease-in-out',
-      once: false, 
+      once: false,
     })
   }, [])
 
   return (
     <section id='internships' className={styles.container}>
-      <h1 className='sectionTitle'>Internships 💼</h1>
-      <h3 className={styles.workExperience}>
-        Total Work Ex: 8 months</h3>
+      <h1 className='sectionTitle'>Work 💼</h1>
+      <h3 className={styles.workExperience}>Total Work Ex: 10 months</h3>
       <div className={styles.internshipCards}>
+        <InternshipCard
+          logoSrc={noxAlgologo}
+          companyName='NOXALGO'
+          position='Development Team Lead'
+          duration='Dec 2024 - Feb 2025'
+          aosEffect='fade-down'
+        />
         <InternshipCard
           logoSrc={deepCytesLogo}
           companyName='DeepCytes Cyber Labs (UK)'
           position='Cybersecurity Analyst/Fellow'
           duration='June 2024 - Dec 2024'
-          aosEffect='fade-up'
+          aosEffect='fade-down'
         />
         <InternshipCard
           logoSrc={stallionLogo}
